c27dde5ff413cf2cdfed22b0136c9a97fb28bb87
Florian Kimmel <fkimmel@adminisatorsMBP.fritz.box>
2022-12-27 21:07:54 +0100

    Make code more readable

--- podcast/src/podcast.tsx
+++ podcast/src/podcast.tsx
@@ -2,6 +2,8 @@ import {Action, ActionPanel, List} from "@raycast/api";
 import * as fs from 'fs';
 import {useEffect, useState} from "react";
 
+const podcastRoot = '/Users/fkimmel/Dropbox/Podcast/Aufnahmen';
+
 export default function PodcastCommand() {
     const [podcast,setPodcast] = useState({ episodes: new Array<string>() });
 
@@ -13,13 +15,12 @@ export default function PodcastCommand() {
     }, [])
 
     return (
-        <List
-            isLoading={podcast.episodes?.length === 0}
-            searchBarPlaceholder="Filter by name ..."
-        >
-            {podcast.episodes.map(
-                (episode) => <EpisodeItem key={episode} name={episode}/>
-            )}  
+        <List isLoading={podcast.episodes?.length === 0}
+              searchBarPlaceholder="Filter by name ...">
+            { podcast
+                .episodes
+                .map((episode) => <EpisodeItem key={episode} name={episode}/>)
+            }  
         </List>
 
     );
@@ -27,9 +28,8 @@ export default function PodcastCommand() {
 
 
 async function readEpisodes() {
-    const testFolder = '/Users/fkimmel/Dropbox/Podcast/Aufnahmen/';
 
-    const files = fs.promises.readdir(testFolder)
+    const files = fs.promises.readdir(podcastRoot)
         .then(files => files.filter((file) => !file.startsWith(".")));
 
     return files
@@ -44,9 +44,9 @@ function EpisodeItem({ name } : EpisodeItemProps) {
         <List.Item
             title={name}
             actions={
-            <ActionPanel title="Automation">
-                <Action title="Run" onAction={() => exeAutomation(name)} />
-            </ActionPanel>
+                <ActionPanel title="Automation">
+                    <Action title="Run" onAction={() => exeAutomation(name)} />
+                </ActionPanel>
             }
          />
     )
@@ -54,6 +54,6 @@ function EpisodeItem({ name } : EpisodeItemProps) {
 
 const exeAutomation = (name : string) => {
     const exec = require('child_process').execSync;
-    const command = `cd /Users/fkimmel/Dropbox/Podcast/Aufnahmen/${name} && podcast ${name}.m4a && exit`
+    const command = `cd ${podcastRoot}/${name} && podcast ${name}.m4a && exit`
     exec(`osascript -e 'tell app "Terminal" to do script "${command}"'`)
 }
